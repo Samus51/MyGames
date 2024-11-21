7e31fa7717a999bfff995043f60675c7460942eb
jorgebenitezz05 <jorgeylenia05@gmail.com>
2024-11-21 11:05:22 +0100

    resuelvo a conflicto

--- RepoMyGames/MyGamesApp/src/main/java/mainApp/Launcher.java
+++ RepoMyGames/MyGamesApp/src/main/java/mainApp/Launcher.java
@@ -4,7 +4,8 @@ package mainApp;
  * Clase launcher
  */
 public class Launcher {
-  public static void main(String[] args) {
-    Main.main(args);
-  }
+public static void main(String[] args) {
+	Main.main(args);
+//Comentario
+}
 }
