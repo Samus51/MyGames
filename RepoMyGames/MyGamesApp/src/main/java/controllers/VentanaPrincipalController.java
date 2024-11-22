ac61d30b91e232e1ed50e5230c06a41bb50913e3
jorgebenitezz05 <jorgeylenia05@gmail.com>
2024-11-22 13:36:20 +0100

    Prueba Login

--- RepoMyGames/MyGamesApp/src/main/java/controllers/VentanaPrincipalController.java
+++ RepoMyGames/MyGamesApp/src/main/java/controllers/VentanaPrincipalController.java
@@ -1,4 +1,4 @@
-package controlers;
+package controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -7,14 +7,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-public class VentanaPrincipalController implements Initializable {
+public class VentanaPrincipalController implements Initializable{
 
   @FXML
   private BorderPane VentanaPrincipal;
@@ -29,30 +27,21 @@ public class VentanaPrincipalController implements Initializable {
   private ImageView imgOjo;
 
   @FXML
-  private Label lblPassword;
-
-  @FXML
-  private Label lblRegistro;
-
-  @FXML
   private Label lblUser;
+
+  @FXML
+  private Label lblUser1;
 
   @FXML
   private BorderPane panelLogo;
 
   @FXML
-  private PasswordField passwordText;
-
-  @FXML
   private TextField txtPasswordClear;
-
-  @FXML
-  private TextField txtUser;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     Image img = new Image(getClass().getResourceAsStream("/Logo.png"));
     imgLogo.setImage(img);
-
   }
+
 }
